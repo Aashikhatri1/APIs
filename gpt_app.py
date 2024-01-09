9e45f091621eb171b94304f9d3d82fa0853e0b65
Aashikhatri1 <134989740+Aashikhatri1@users.noreply.github.com>
2024-01-09 17:29:18 +0530

    Update gpt_app.py

--- gpt_app.py
+++ gpt_app.py
@@ -8,8 +8,56 @@ app = Flask(__name__)
 # db = client.Meeting_automation
 # collection = db.meeting_link
 
-@app.route('/postdata', methods=['POST'])
-def post_data():
+@app.route('/sales_agent', methods=['POST'])
+def sales_agent():
+    # Parse data from request
+    data = request.data.decode('utf-8')
+    print(data)
+
+    # Save data to MongoDB
+    # collection.insert_one(data)
+
+    # Send response
+    return jsonify({'message': 'information noted'})
+
+@app.route('/customer_support', methods=['POST'])
+def customer_support():
+    # Parse data from request
+    data = request.data.decode('utf-8')
+    print(data)
+
+    # Save data to MongoDB
+    # collection.insert_one(data)
+
+    # Send response
+    return jsonify({'message': 'information noted'})
+
+@app.route('/habit_coach', methods=['POST'])
+def habit_coach():
+    # Parse data from request
+    data = request.data.decode('utf-8')
+    print(data)
+
+    # Save data to MongoDB
+    # collection.insert_one(data)
+
+    # Send response
+    return jsonify({'message': 'information noted'})
+
+@app.route('/PA', methods=['POST'])
+def PA():
+    # Parse data from request
+    data = request.data.decode('utf-8')
+    print(data)
+
+    # Save data to MongoDB
+    # collection.insert_one(data)
+
+    # Send response
+    return jsonify({'message': 'information noted'})
+
+@app.route('/sales_insights', methods=['POST'])
+def sales_insights():
     # Parse data from request
     data = request.data.decode('utf-8')
     print(data)
